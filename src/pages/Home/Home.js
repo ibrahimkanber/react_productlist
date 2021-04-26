b0f182eb84b2e8f6e1ddd5280b974b2dfadb414d
ibrahim <ikanber45@gmail.com>
2021-04-26 11:32:13 +0200

    init

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Card from "../../components/Card/Card"
+import "./Home.css"
+
+function Home() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Home
