aeb3d41ed793e1264d10c54e8393c6ee55c3c06a
ibrahim <ikanber45@gmail.com>
2021-04-26 13:07:18 +0200

    final

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import "./Card.css"
-function Card() {
+
+/* id: 0
+imgURL: "https://m.media-amazon.com/images/I/51uguxa9nYL._AC._SR360,460.jpg"
+inStock: true
+price: "₺134,77"
+title: " */
+
+
+function Card({product}) {
+
+    const timer=setTimeout(()=>{
+        console.log("timer calisti")
+    },5000)
+    
+    useEffect(() => {
+        console.log("effect")
+        return () => {
+            clearTimeout(timer)
+        }
+    }, [])
     return (
-        <div>
-            
+        <div className="card">
+            <img  src={product.imgURL} className="img" alt={product.title}/>
+            <p>{product.title}</p>
+            <p>{product.price}</p>
+            <p style={{color:product.inStock ?"green" :"red"}}>{product.inStock ? "available" :"unavailable"}</p>
         </div>
     )
 }
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,64 @@
-import React from 'react'
-import Card from "../../components/Card/Card"
-import "./Home.css"
+import Card from "../../components/Card/Card";
+import "./Home.css";
+import data from "../../data.json";
+import { useState, useEffect } from "react";
 
 function Home() {
-    return (
-        <div>
-            
-        </div>
-    )
+  const [searchedValue, setSearchedValue] = useState("");
+  const [filteredData, setFilteredData] = useState(data);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(searchedValue);
+
+    const fiteredProducts = data.filter((product) => {
+      const title = product.title.toUpperCase();
+      const text = searchedValue.toUpperCase();
+
+      return title.includes(text);
+    });
+    setFilteredData(fiteredProducts);
+  };
+
+  const handleChange = (e) => {
+    setSearchedValue(e.target.value);
+  };
+
+  useEffect(() => {
+    const fiteredProducts = data.filter((product) => {
+      const title = product.title.toUpperCase();
+      const text = searchedValue.toUpperCase();
+
+      return title.includes(text);
+    });
+    setFilteredData(fiteredProducts);
+
+    console.log("at kafasi")
+
+
+  }, [searchedValue]);
+
+  return (
+    <div style={{ backgroundColor: "red" }}>
+      <div className="search">
+        <form className="form" onSubmit={handleSubmit}>
+          {/* <input type="text" onChange={handleChange}/> */}
+          <input
+            type="text"
+            onChange={handleChange}
+            placeholder="useEffect example"
+          />
+          <button type="submit">Search</button>
+        </form>
+      </div>
+      <div className="productList">
+        {filteredData.map((product) => (
+          <Card product={product} key={product.id} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
